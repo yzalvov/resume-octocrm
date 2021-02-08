fd814ecfbebc33a966f2f07a00d2cbaa9ae08451
Yuri Z <yzalvov@gmail.com>
2021-02-08 19:29:20 +0300

    Fix page scrolling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import React from 'react'
 import styled, { createGlobalStyle } from 'styled-components'
-import { Grommet as OriginalGrommet, Main, ResponsiveContext } from 'grommet'
+import { Grommet as OriginalGrommet, Main } from 'grommet'
 import {
   colors,
   theme,
@@ -41,50 +41,44 @@ function App() {
   return (
     <GlobalContextProvider>
       <FirebaseAuth />
-      <Grommet full theme={theme} background={background} key={background}>
+      <Grommet theme={theme} background={background} key={background}>
         <HtmlOverscrollBackround background={background} />
-        <ResponsiveContext.Consumer>
-          {size => (
-            <ScreenWidthContainer>
-              <Router>
-                {/* <AppBar /> */}
-                {/* <Route path="*" render={() => <AppBar />} /> */}
-                <Route path="*" component={AppBar} />
-                <Main>
-                  <Switch>
-                    <Route exact path={routes.OFFER} component={OfferScreen} />
-                    <Route
-                      exact
-                      path={routes.DASHBOARD}
-                      component={withAuthor(PartnerDashboardScreen)}
-                    />
-                    <Route
-                      exact
-                      path={routes.SIGN_UP_OFFER}
-                      component={withAuthen(SignUpOfferScreen)}
-                    />
-                    <Route
-                      exact
-                      path={routes.REVIEWS}
-                      component={withAuthor(ReviewsArchiveScreen)}
-                    />
-                    <Route
-                      exact
-                      path={routes.SIGN_IN}
-                      component={withAuthen(SignInScreen)}
-                    />
-                    <Route
-                      exact
-                      path={routes.VISITS}
-                      component={withAuthor(VisitsHistoryScreen)}
-                    />
-                  </Switch>
-                </Main>
-                <Route path="*" component={Footer} />
-              </Router>
-            </ScreenWidthContainer>
-          )}
-        </ResponsiveContext.Consumer>
+        <ScreenWidthContainer>
+          <Router>
+            <Route path="*" component={AppBar} />
+            <Main>
+              <Switch>
+                <Route exact path={routes.OFFER} component={OfferScreen} />
+                <Route
+                  exact
+                  path={routes.DASHBOARD}
+                  component={withAuthor(PartnerDashboardScreen)}
+                />
+                <Route
+                  exact
+                  path={routes.SIGN_UP_OFFER}
+                  component={withAuthen(SignUpOfferScreen)}
+                />
+                <Route
+                  exact
+                  path={routes.REVIEWS}
+                  component={withAuthor(ReviewsArchiveScreen)}
+                />
+                <Route
+                  exact
+                  path={routes.SIGN_IN}
+                  component={withAuthen(SignInScreen)}
+                />
+                <Route
+                  exact
+                  path={routes.VISITS}
+                  component={withAuthor(VisitsHistoryScreen)}
+                />
+              </Switch>
+            </Main>
+            <Route path="*" component={Footer} />
+          </Router>
+        </ScreenWidthContainer>
       </Grommet>
     </GlobalContextProvider>
   )
